b9069e1927ccf0c2dd5fa922df7cea4aa821c789
郑忠 <529931497@qq.com>
2018-01-17 12:51:02 +0800

    网络视频爬虫系统初始化

---
+++ src/main/java/com/symphony/supervideo/SuperVideoApplication.java
@@ -0,0 +1,13 @@
+package com.symphony.supervideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperVideoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperVideoApplication.class, args);
+	}
+
+}

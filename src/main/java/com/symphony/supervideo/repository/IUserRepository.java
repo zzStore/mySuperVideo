69713c705e8bd0e00f9349fd6536d1423face08b
郑忠 <529931497@qq.com>
2018-01-22 12:16:38 +0800

    网络视频爬虫系统第一版本

--- src/main/java/com/symphony/supervideo/repository/IUserRepository.java
+++ src/main/java/com/symphony/supervideo/repository/IUserRepository.java
@@ -5,11 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author zz.
- * @2018/1/17 14:34.
+ * 2018/1/17 14:34.
  * 用户信息CRUD接口
  */
 
 public interface IUserRepository extends JpaRepository<UserInfo,Integer> {
-
 
 }
